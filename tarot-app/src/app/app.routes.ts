f6c42a2aa719c1303f351c97a3656666cee670d0
Amanda <amanda.winkler@penske.com>
2025-09-19 11:06:42 -0400

    save

--- tarot-app/src/app/app.routes.ts
+++ tarot-app/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
-import { TarotOneCardComponent} from './core/components/tarot-one-card/tarot-one-card.component';
+import { TarotOneCardComponent } from './core/components/tarot-one-card/tarot-one-card.component';
 export const routes: Routes = [
-    { path: 'tarot', component: TarotOneCardComponent },
-{ path: '', redirectTo: '/tarot', pathMatch: 'full' },
-{ path: '**', redirectTo: '/tarot' }
+  { path: 'tarot', component: TarotOneCardComponent },
+  { path: '', redirectTo: '/tarot', pathMatch: 'full' },
 ];
